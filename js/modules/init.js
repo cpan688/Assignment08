3b1bb6790c3eb3b1ec5c6023c2937f09d74b59e3
TheCircle <164573814+box8988@users.noreply.github.com>
2025-04-25 23:02:05 -0700

    Added init.js for loading the JSON data

---
+++ js/modules/init.js
@@ -0,0 +1,6 @@
+// USE async/await TO LOAD THE JSON DATA AND RETURN IT BACK INTO THE buildGrid() FUNCTION
+
+/* Fetching the data from the module will return a Promise object.
+You will need to either use another async/await within the buildGrid() function 
+OR, you will need to use .then() to process the return JSON object */
+
